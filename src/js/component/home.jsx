09c785169136f83c6167ccde0db33ddb3cb28331
jsowers13 <jsowers13@gmail.com>
2022-09-28 23:21:45 +0000

    first and last

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [todos, setToDos] = useState(["wash my hands", "make the bed"]);
+  const [newToDo, setnewToDo] = useState("");
+  function handleInput(event) {
+    event.preventDefault();
+    setnewToDo(event.target.value);
+    console.log(newToDo);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let newArray = [...todos];
+    newArray.push(newToDo);
+    setToDos(newArray);
+    setnewToDo("");
+  }
+  function deleteToDo(index) {
+    let newArr = [...todos];
+    newArr.splice(index, 1);
+    setToDos(newArr);
+  }
+
+  return (
+    <div className=" container text-center">
+      <h1 className="display-4">todos</h1>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="form-control form-control-lg"
+            type="text"
+            placeholder="Add To Do Here"
+            aria-label=".form-control-lg example"
+            onChange={handleInput}
+            value={newToDo}
+          />
+        </form>
+
+        <ul className="list-group text-start">
+          {todos.map((item, index) => {
+            return (
+              <li key={index} className="list-group-item">
+                {item}
+
+                <span className="float-end">
+                  <i className="fa-solid fa-trash-can" onClick={deleteToDo}></i>
+                </span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
